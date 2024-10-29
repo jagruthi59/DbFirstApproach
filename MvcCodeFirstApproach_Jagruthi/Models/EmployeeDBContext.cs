d335ff7be3ed21bdb94c904a53a5a687a90ffc8c
jagruthi59 <jagruthi.yellavula@gmail.com>
2024-10-29 15:23:30 +0530

    first

---
+++ MvcCodeFirstApproach_Jagruthi/Models/EmployeeDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcCodeFirstApproach_Jagruthi.Models
+{
+    public class EmployeeDBContext:DbContext
+    {
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext>options):base(options)
+        { 
+        }
+        public DbSet<EmployeeModel> employees { get; set; }
+    }
+}
---
+++ MvcCodeFirstApproach_Jagruthi/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MvcCodeFirstApproach_Jagruthi.Models
+{
+    [Table("tblEmployee")]
+    public class EmployeeModel
+    {
+        [Key]
+        public int id { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeePhone { get; set; }
+        public string Job{ get; set; }
+        public double Salary { get; set; }
+    }
+}
